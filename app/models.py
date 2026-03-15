899b6acd2bddbb42e79f6426f0cc5ec613ca5318
Evans-Boateng <eb5337877@gmail.com>
2026-03-15 06:57:14 +0000

    creates user model and schemas

--- app/models.py
+++ app/models.py
@@ -1,5 +1,21 @@
 from sqlmodel import SQLModel, Field
+from pydantic import EmailStr
+import uuid
 
-class User(SQLModel, table=True):
-  id: int = Field(unique=True, primary_key=True)
-  name: str
+
+class UserBase(SQLModel):
+  username: str = Field(unique=True)
+  email: EmailStr = Field(index=True, unique=True)
+  full_name: str
+
+class User(UserBase, table=True):
+  id: uuid.UUID | None = Field(default_factory=uuid.uuid4, unique=True, primary_key=True)
+  hashed_password: str
+
+class UserCreate(UserBase):
+  password: str
+  
+class UserOut(UserBase):
+  id: uuid.UUID
+  
+
