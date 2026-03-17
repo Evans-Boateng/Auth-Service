5ed6c47eaf159dad4a4ac94a93dbfb324079f453
Evans-Boateng <eb5337877@gmail.com>
2026-03-17 09:44:57 +0000

    adds utility function to get and authenticate users.

--- app/core/security.py
+++ app/core/security.py
@@ -1,4 +1,15 @@
 from pwdlib import PasswordHash
+from fastapi import HTTPException
+from sqlmodel import select
+from datetime import datetime
+from ..models import User
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DUMMY_HASH = os.getenv("DUMMY_HASH")
+
 
 password_hash = PasswordHash.recommended()
 
@@ -8,3 +19,26 @@ def harsh_password(password):
 
 def verify_password(plain_password, hashed_password):
     return password_hash.verify(plain_password, hashed_password)
+
+def get_user(username, session):
+    statement = select(User).where(User.username == username)
+    user = session.exec(statement).first()
+    return user
+
+def authenticate_user(username: str, password: str, session):
+    user = get_user(username, session)
+    if not user:
+       #here, were making sure that the endpoint takes the same time to respond, whether the user is valid or not
+       #and that kinda confuse attackers by preventing timing attacks that could be use identify which username they tried is an actual user
+       verify_password(plain_password=password, hashed_password=DUMMY_HASH)
+       return False
+    if not verify_password(plain_password=password, hashed_password=user.hashed_password):
+        return False
+    return user
+
+# def create_token(data: dict, expires_delta: datetime, type: str):
+#     to_encode = data.copy()
+    
+#     if expires_delta: 
+#         expire = 2
+    
