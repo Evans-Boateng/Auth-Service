ee0efefad855f1a38305212942d31f52a55e654d
Evans-Boateng <eb5337877@gmail.com>
2026-05-28 01:40:09 +0000

    Add schemas directory

---
+++ app/schemas/user.py
@@ -0,0 +1,29 @@
+from datetime import datetime, timezone
+import uuid
+from pydantic import EmailStr, Field, BaseModel
+from sqlmodel import SQLModel
+
+
+class UserBase(SQLModel):
+  username: str = Field(index=True, unique=True)
+  email: EmailStr = Field(index=True, unique=True)
+  full_name: str
+
+class UserCreate(UserBase):
+  password: str
+  
+class UserOut(UserBase):
+  id: uuid.UUID
+
+class Token(BaseModel):
+  access_token: str
+  refresh_token: str | None = None
+  token_type: str
+  access_token_exiry: datetime
+  refresh_token_expiry: datetime
+
+class Refresh_Token(BaseModel):
+  refresh_token: str
+
+class Access_Token(BaseModel):
+  access_token: str
