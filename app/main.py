f292bf806b38bf7d86171d7d5d78ab5a82e91925
Evans-Boateng <eb5337877@gmail.com>
2026-06-06 07:33:29 +0000

    Add endpoint for admin login with client credentials

--- app/main.py
+++ app/main.py
@@ -4,16 +4,22 @@ from fastapi.exceptions import HTTPException
 from fastapi.responses import Response
 from sqlmodel import Session, select, delete
 from typing import Annotated
+
+from app.schemas.client import ClientIn
 from .database import create_db_and_tables
-from .dependencies import get_session, check_limit
-from .models import User, UserCreate, Token, RefreshToken, Refresh_Token, Access_Token, Client, Role
-from .core.security import harsh_password, authenticate_user, create_token, hash_token, verify_token, generate_client_credentials
+from .dependencies import SessionDp, get_session, check_limit
+from .models import User, RefreshToken, Client, Role, UserRole
+from .schemas.user import UserCreate, Token, Refresh_Token, Access_Token
+from .core.security import harsh_password, authenticate_user, create_token, hash_token, verify_password, verify_token, generate_client_credentials
 from contextlib import asynccontextmanager
 from datetime import datetime, timedelta, timezone
 import hashlib
 from jwt import InvalidTokenError
 from pyrate_limiter import Rate, Duration
 import uuid
+from uuid import UUID
+from sqlalchemy.exc import IntegrityError
+from .routers import users
 
 # @asynccontextmanager
 # async def lifespan(app: FastAPI):
@@ -23,8 +29,6 @@ import uuid
 app = FastAPI()
 
 router = APIRouter(prefix="/auth")
-
-SessionDp = Annotated[Session, Depends(get_session)]
 
 #testing
 @router.get("/test", status_code=200)
@@ -57,7 +61,43 @@ async def create_new_client(name: str, session: SessionDp):
 
   return {"client_id": client_id, "client_secret": client_secret}
 
-@router.post("/admin/clients/roles", status_code=status.HTTP_204_NO_CONTENT)
+@router.post("/admin/clients/token", response_model=Access_Token, status_code=status.HTTP_200_OK)
+async def login_admin(request_data: ClientIn, session: SessionDp):
+  credentails_exception = HTTPException(
+    detail="Invalid client",
+    status_code=status.HTTP_401_UNAUTHORIZED
+  )
+  client = session.exec(
+    select(Client).where(Client.id == request_data.client_id)
+  ).first()
+  if not client:
+    raise credentails_exception
+  
+  if not verify_password(plain_password=request_data.client_secret, hashed_password=client.hashed_secret):
+    raise credentails_exception
+  
+  if request_data.grant_type == "client_credentials":
+    access_token_expiry = timedelta(minutes=1)
+
+    access_token = create_token(
+      data={
+        "sub": str(client.id),
+        "iss": client.name,
+        "type": "access",
+        "role": "master"
+      },
+      expires_delta=access_token_expiry,
+      type="access"
+    )
+    return (Access_Token(access_token=access_token))
+  
+  raise HTTPException(
+    detail="Invalid grant type",
+    status_code=status.HTTP_400_BAD_REQUEST
+  )
+    
+
+@router.post("/admin/clients/roles", status_code=status.HTTP_200_OK)
 async def create_new_role(name: str, client_id: str, session: SessionDp):
 
   #check if client exists
@@ -74,20 +114,29 @@ async def create_new_role(name: str, client_id: str, session: SessionDp):
   try:
     new_role = Role(
       name = name,
-      client_id=client.id
+      client = client
     )
+    session.add(new_role)
+    session.commit()
   except Exception as e:
     raise HTTPException(
       detail="Cannot create multiple roles with the same",
-      status_code= status.HTTP_400_BAD_REQUEST
+      status_code= status.HTTP_409_CONFLICT
     )
-  session.add(new_role)
-  session.commit()
 
-  return
+  return {"role": new_role.name, "client": new_role.client_id}
+
+@router.delete("/admin/clients/{client_name}/delete")
+async def delete_client(client_name:str, session: SessionDp):
+  session.exec(
+    delete(Client).where(Client.name == client_name)
+  )
+  session.commit()
+  
+  return {"message": "Client deleted"}
 
 @router.post("/admin/clients/{client_id}/users/{user_id}/roles")
-async def assign_role(client_id: str, user_id: str, role_name: str, session: SessionDp):
+async def assign_role(client_id: str, user_id: UUID, roles: list[str], session: SessionDp):
   unauthorized_exception = HTTPException(
     status_code=status.HTTP_400_BAD_REQUEST,
     detail="User does not exist"
@@ -98,200 +147,40 @@ async def assign_role(client_id: str, user_id: str, role_name: str, session: Ses
     raise unauthorized_exception
   
   client = session.get(Client, client_id)
+  if not client:
+    raise HTTPException(
+      detail="Client does not exist",
+      status_code=status.HTTP_401_UNAUTHORIZED
+    )
   
-  role = session.exec(
-    select(Role).where(Role.name == role_name)
-  ).first()
+  for role in roles:
+    role_in_db = session.exec(
+      select(Role).where(Role.name == role)
+    ).first()
 
-  if role not in client.roles:
-    raise unauthorized_exception
+    if role_in_db not in client.roles:
+      raise HTTPException(
+        detail=f"The role {role} does not exist",
+        status_code=status.HTTP_409_CONFLICT
+      )
+    try:
+      user_role = UserRole(
+        user_id=user_id,
+        role_id = role_in_db.id
+      )
+      session.add(user_role)
+      session.commit()
+      session.refresh(user_role)
+    except IntegrityError:
+      session.rollback()
+      raise HTTPException(
+        detail=f"The role {role} is already assigned to user",
+        status_code = status.HTTP_400_BAD_REQUEST
+      )
 
-  return
+  return {"roles":roles, "client": client.roles }
 
 
-@router.post("/register", status_code=status.HTTP_204_NO_CONTENT)
-async def create_user(data: Annotated[UserCreate, Form()], session: SessionDp):
-  credentails_exception = HTTPException(
-    status_code = status.HTTP_400_BAD_REQUEST,
-    detail= "Username or email already exists"
-  )
-
-  #now we hash the password and validate the request data with model_validate
-  hashed_password = harsh_password(data.password)
-  user = User.model_validate(data, update={"hashed_password": hashed_password})
-
-  #check if the username or email already exists
-  existing_username = session.exec(
-    select(User).where(User.username == data.username)
-  ).first()
-  
-  existing_email = session.exec(
-    select(User).where(User.email == data.email)
-  ).first()
-
-  if existing_username or existing_email:
-    raise credentails_exception
-  
-  session.add(user)
-  session.commit()
-  session.refresh(user)
-
-@router.post("/token", response_model=Token, dependencies=[Depends(check_limit(Rate(5, Duration.MINUTE * 15)))])
-async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], session: SessionDp):
-  credentials_exception = HTTPException(
-    status_code=status.HTTP_400_BAD_REQUEST,
-    detail="Invalid username or password"
-  )
-  user = authenticate_user(username=form_data.username, password=form_data.password, session=session)
-  if not user: 
-    raise credentials_exception
-  
-  access_token_expiry = timedelta(minutes=7)
-  refresh_token_expiry = timedelta(days=7)
-
-  access_token = create_token(
-    data={
-      "sub": str(user.id),
-      "username": user.username,
-      "email": user.email,
-      "type": "access"
-    },
-    expires_delta=access_token_expiry,
-    type="access"
-  )
-  refresh_token = create_token(
-    data={
-      "sub": str(user.id),
-      "type": "refresh"
-    },
-    expires_delta=refresh_token_expiry,
-    type="refresh"
-  )
-  
-  refresh_token_in_db = RefreshToken(
-    hashed_token=hash_token(refresh_token),
-    user_id=user.id,
-    expires_at = datetime.now() + refresh_token_expiry
-  )
-
-  session.add(refresh_token_in_db)
-  session.commit()
-  token = Token(access_token=access_token, refresh_token=refresh_token, token_type="Bearer", access_token_exiry=datetime.now() + access_token_expiry, refresh_token_expiry=datetime.now() + refresh_token_expiry)
-  return token
-
-@router.post("/token/refresh", response_model=Token, dependencies=[Depends(check_limit(Rate(20, Duration.HOUR * 1)))])
-async def refresh_token(request_data: Refresh_Token, session: SessionDp, response: Response):
-  credentials_exception = HTTPException(
-    status_code=status.HTTP_403_FORBIDDEN,
-    detail="Could not validate credentials"
-  )
-  try:
-    payload = verify_token(request_data.refresh_token)
-    if payload.get("type") != "refresh":
-      raise credentials_exception
-  except InvalidTokenError:
-    raise credentials_exception
-
-  refresh_in_db = session.exec(
-    select(RefreshToken).where(RefreshToken.hashed_token == hash_token(request_data.refresh_token))
-  ).first()
-
-  if not refresh_in_db or refresh_in_db.is_revoked:
-    raise credentials_exception
-  
-  #here we check if the user actually exists in the db
-  user = session.get(User, refresh_in_db.user_id)
-  if not user:
-    raise credentials_exception
-  
-  #create the new access token and refresh token and delete the old refresh token(refresh rotation)
-  access_token_expiry = timedelta(minutes=7)
-  refresh_token_expiry = timedelta(days=7)
-
-  new_access_token = create_token(
-    data={
-      "sub": str(user.id),
-      "username": user.username,
-      "email": user.email,
-      "type": "access"
-    },
-    expires_delta= access_token_expiry,
-    type="access"
-  )
-  new_refresh_token = create_token(
-    data={
-      "sub": str(user.id),
-      "type": "refresh"
-    },
-    expires_delta= refresh_token_expiry,
-    type="refresh"
-  )
-  
-  session.delete(refresh_in_db)
-
-  new_stored_refresh_token = RefreshToken(
-    user_id=user.id,
-    hashed_token=hash_token(new_refresh_token),
-    expires_at = datetime.now() + refresh_token_expiry
-  )
-  session.add(new_stored_refresh_token)
-  session.commit()
-
-  token = Token(access_token=new_access_token, refresh_token=new_refresh_token, access_token_exiry= datetime.now() + access_token_expiry, refresh_token_expiry=datetime.now() + refresh_token_expiry, token_type="Bearer") 
-
-  return token
-
-@router.post("/logout", dependencies=[Depends(check_limit(Rate(5, Duration.MINUTE * 15)))])
-async def logout(request_data: Refresh_Token, session: SessionDp):
-  access_exception = HTTPException(
-    status_code=401,
-    detail = "Access denied"
-  )
-
-  try:
-    payload = verify_token(request_data.refresh_token)
-    if payload.get("type") != "refresh":
-      raise access_exception
-  except InvalidTokenError:
-    raise access_exception
-  
-  refresh_in_db = session.exec(
-    select(RefreshToken).where(RefreshToken.hashed_token == hash_token(request_data.refresh_token))
-  ).first()
-  if not refresh_in_db or refresh_in_db.is_revoked:
-    raise access_exception
-  
-  #revoke the token and commit to database
-  refresh_in_db.is_revoked = True
-  session.add(refresh_in_db)
-  session.commit()
-
-  return "User logged out successfully"
-
-@router.post("/logout-all")
-async def logout_all(request_data: Access_Token, session: SessionDp):
-  """
-  This endpoint basically logs users out from all their devices -
-  thus all refresh tokens are deleted from the db
-  """
-  access_exception = HTTPException(
-    status_code=401,
-    detail = "Access denied"
-  )
-
-  try:
-    payload = verify_token(request_data.access_token)
-    if payload.get("type") != "access":
-      raise access_exception
-  except InvalidTokenError:
-    raise access_exception
-  
-  #delete all the refresh tokens associated with the user
-  session.exec(
-    delete(RefreshToken).where(RefreshToken.user_id == uuid.UUID(payload.get("sub")))
-  )
-  session.commit()
-
-  return "User logged out from all devices successfully"
 
 app.include_router(router)
+app.include_router(users.router)
