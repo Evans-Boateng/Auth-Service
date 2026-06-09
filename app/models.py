76455d73f23aae41f68fbf4c6b52ded9b3c75f93
Evans-Boateng <eb5337877@gmail.com>
2026-06-09 08:27:49 +0000

    Add created field to the user model

--- app/models.py
+++ app/models.py
@@ -3,6 +3,8 @@ from sqlmodel import SQLModel, Field, Relationship, UniqueConstraint
 from pydantic import EmailStr, BaseModel
 import uuid
 
+from app.schemas.user import UserBase
+
 
 class Client(SQLModel, table=True):
   id: str = Field(primary_key=True, unique=True)
@@ -20,6 +22,8 @@ class Role(SQLModel, table=True):
 
 
 class UserRole(SQLModel, table=True):
+  __tablename__ = "user_roles"
+
   user_id: uuid.UUID = Field(foreign_key="user.id", ondelete="CASCADE", primary_key=True)
   role_id: uuid.UUID = Field(foreign_key="role.id", ondelete="CASCADE", primary_key=True)
 
@@ -30,34 +34,12 @@ class RefreshToken(SQLModel, table=True):
   user_id: uuid.UUID = Field(foreign_key="user.id", ondelete="CASCADE")
   expires_at: datetime
   is_revoked: bool = False
-  
-
-class UserBase(SQLModel):
-  username: str = Field(index=True, unique=True)
-  email: EmailStr = Field(index=True, unique=True)
-  full_name: str
 
 class User(UserBase, table=True):
   id: uuid.UUID | None = Field(default_factory=uuid.uuid4, unique=True, primary_key=True)
   hashed_password: str
   refresh_tokens: RefreshToken | None = Relationship(back_populates="user")
+  created_at: datetime | None = Field(default_factory=datetime.now)
+  updated_at: datetime | None = Field(default_factory=datetime.now)
 
-class UserCreate(UserBase):
-  password: str
-  
-class UserOut(UserBase):
-  id: uuid.UUID
-
-class Token(BaseModel):
-  access_token: str
-  refresh_token: str | None = None
-  token_type: str
-  access_token_exiry: datetime
-  refresh_token_expiry: datetime
-
-class Refresh_Token(BaseModel):
-  refresh_token: str
-
-class Access_Token(BaseModel):
-  access_token: str
 
